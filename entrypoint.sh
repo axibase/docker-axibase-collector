59896e9699d22deae80f8c0df2b59ce31738cf68
Unrealwork <unrealwork@gmail.com>
2017-01-11 17:28:06 +0300

    entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -19,11 +19,13 @@ fi
 
 cd ${SCRIPTS_HOME}
 echo "Starting services ..."
+#Create empty cron job
 touch /etc/cron.d/root
 chmod +x /etc/cron.d/root
-rsyslogd
 printf "# Empty line\n" >> /etc/cron.d/root
 crontab /etc/cron.d/root
+#Enable system logging
+rsyslogd
 
 cron -f &
 ./start-collector.sh "$args"
