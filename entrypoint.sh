9139b856d845cbc68e8874c94bb989bf34ccf658
Sergei Rodionov <sergei.rodionov@axibase.com>
2017-09-28 12:39:08 +0300

    lang fixes

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,13 +13,13 @@ else
 fi
 
 if [ `whoami` != "$owner" ]; then
-    echo "Current user: `whoami`. Expecting user: $owner. User mismatch, switching user to $owner."
+    echo "Current user: `whoami`. Installation user: $owner. Switching user to $owner."
     su -c "$SCRIPT $args" "$owner"
     exit $?
 fi
 
 cd ${SCRIPTS_HOME}
-echo "Starting collection service ..."
+echo "Starting Axibase Collector ..."
 
 for i in $args; do
     case $i in
@@ -40,14 +40,14 @@ done
 error="false"
 function validate_hostname {
     if [[ $1 == *"_"* ]]; then
-        echo "Error: hostname in the '$2' contains underscore"
+        echo "Error: Hostname in '$2' is not valid (contains an underscore)."
         error="true"
     fi
 }
 
 function validate_password {
     if [[ ${#1} < 6 ]]; then
-        echo "Error: password in the '$2' contains less than 6 characters"
+        echo "Error: Password in '$2' contains less than 6 characters."
         error="true"
     fi
 }
@@ -61,7 +61,7 @@ if [[ ${ATSD_URL} =~ $URL_PATTERN ]]; then
 fi
 
 if [ "$error" = "true" ]; then
-    echo "Collection service failed to start"
+    echo "Axibase Collector failed to start"
     exit 0
 fi
 
@@ -84,6 +84,6 @@ while [ "$executing" = "true" ]; do
 done
 
 #Stop collector
-echo "Stopping collection service ..."
+echo "Stopping Axibase Collector ..."
 ./stop-collector.sh "$STOP_SIGNAL"
 exit 0
