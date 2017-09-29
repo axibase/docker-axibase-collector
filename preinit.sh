df3deb45b5361f2c9baca03d1f425bfab4a555ca
Valery Tolstov <me@vtolstov.org>
2017-09-29 23:22:34 +0300

    Minor changes
    * Set executable bit for preinit.sh
    * Extract common paths
    * Reorder arguments
    * Fix typo
    * Add EXIT command to force newline after ij output
    * Clean /tmp

--- preinit.sh
+++ preinit.sh
@@ -1,33 +1,36 @@
 #!/usr/bin/env bash
 
+COLLECTOR_HOME=/opt/axibase-collector
+
 # Start Collector to create Derby tables
-/opt/axibase-collector/bin/start-collector.sh
-/opt/axibase-collector/bin/stop-collector.sh
+${COLLECTOR_HOME}/bin/start-collector.sh
+${COLLECTOR_HOME}/bin/stop-collector.sh
 
 # Remove logs and keystore
-rm -rfv /opt/axibase-collector/logs /opt/axibase-collector/conf/keystores/client.keystore
+rm -rfv ${COLLECTOR_HOME}/logs ${COLLECTOR_HOME}conf/keystores/client.keystore
 
 # Download Derby to get ij tool
 DERBY_DOWNLOAD_LINK='http://mirror.linux-ia64.org/apache/db/derby/db-derby-10.12.1.1/db-derby-10.12.1.1-lib.zip'
-wget "${DERBY_DOWNLOAD_LINK}" -O /tmp/derby.zip
+wget -O /tmp/derby.zip "${DERBY_DOWNLOAD_LINK}"
 
 # Cleanup tables
-cd /tmp
-unzip /tmp/derby.zip
+unzip /tmp/derby.zip -d /tmp
 cat <<EOF >/tmp/cleanup_tables.sql
 --connect to the local database
 CONNECT 'jdbc:derby:/opt/axibase-collector/acdb';
---delete link to default ptsd
+--delete link to default atsd
 UPDATE JOB_CONFIG SET ATSD_CONFIGURATION=NULL;
 --delete pool for default atsd
 DELETE FROM CONNECTION_POOL WHERE ID IN (SELECT CONNECTION_POOL_CONFIG FROM ATSD_CONFIGURATION);
 --delete default atsd
 DELETE FROM ATSD_CONFIGURATION;
+EXIT;
 EOF
 
-export CLASSPATH='/tmp/db-derby-10.12.1.1-lib/lib/derby.jar:/tmp/db-derby-10.12.1.1-lib/lib/derbytools.jar'
+DERBY_LIB='/tmp/db-derby-10.12.1.1-lib/lib'
+export CLASSPATH="${DERBY_LIB}/derby.jar:${DERBY_LIB}/derbytools.jar"
 java org.apache.derby.tools.ij /tmp/cleanup_tables.sql
 
 # Remove temporary files
-rm -rvf /tmp/derby* /tmp/db-derby*
 rm $(readlink -f "${BASH_SOURCE[0]}")
+rm -rfv /tmp/*
